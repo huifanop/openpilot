c736864054bbb9a35b0d2687428f7bee35715b1b
huifan <huifan0114@gmail.com>
2026-01-14 07:16:37 +0800

    Update soundd.py

--- selfdrive/ui/soundd.py
+++ selfdrive/ui/soundd.py
@@ -37,6 +37,9 @@ sound_list: dict[Any, tuple[str, int | None, float]] = {
   AudibleAlert.disengage: ("disengage.wav", 1, MAX_VOLUME),
   AudibleAlert.refuse: ("refuse.wav", 1, MAX_VOLUME),
 
+  AudibleAlert.warningSoft: ("warning_soft.wav", None, MAX_VOLUME),
+  AudibleAlert.warningImmediate: ("warning_immediate.wav", None, MAX_VOLUME),
+
   AudibleAlert.prompt: ("prompt.wav", 1, MAX_VOLUME),
   AudibleAlert.promptRepeat: ("prompt.wav", None, MAX_VOLUME),
   AudibleAlert.promptDistracted: ("prompt_distracted.wav", None, MAX_VOLUME),
