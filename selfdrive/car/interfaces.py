cd6c6582f78e1cb47cb385118e52a33f301a4af3
huifan <huifan0114@gmail.com>
2026-01-13 21:35:26 +0800

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -364,7 +364,7 @@ class CarInterfaceBase(ABC):
 
 
   def create_common_events(self, cs_out, extra_gears=None, pcm_enable=True, allow_enable=True,
-                           enable_buttons=(ButtonType.accelCruise, ButtonType.decelCruise)):
+                           enable_buttons=(ButtonType.accelCruise, ButtonType.decelCruise), frogpilot_toggles=None):
     events = Events()
 
     if cs_out.doorOpen:
